615392b4902f4da6227a6b1ecbe75767cf0d0a76
Samim Shammo <you@example.comsamimshammo>
2021-10-31 19:53:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch , Route} from 'react-router-dom';
+import Home from './component/Home/Home/Home';
+import Notfound from './component/Notfound/Notfound';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+import AuthProvider from './context/AuthProvider';
+import Login from './component/Login/Login';
+import PrivateRouter from './PrivateRouter/PrivateRouter';
+import AddService from './component/AddService/AddService';
+import SeeDetails from './component/Home/OurTourPackage/TourDetails/SeeDetails/SeeDetails';
+import ManagePackage from './component/ManagePackage/ManagePackage';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+      <BrowserRouter>
+       <Header></Header>
+          <Switch>
+             <Route exact path="/">
+                  <Home></Home>
+             </Route>
+             <Route exact path="/home">
+                  <Home></Home>
+             </Route>
+             <Route path="/login">
+               <Login></Login>
+             </Route>
+            <PrivateRouter path="/addService">
+               <AddService></AddService>
+            </PrivateRouter>
+            <PrivateRouter path="/managePackage">
+               <ManagePackage></ManagePackage>
+            </PrivateRouter>
+            <PrivateRouter path="/seeDetails/:id">
+              <SeeDetails></SeeDetails>
+            </PrivateRouter>
+             <PrivateRouter path="*">
+               <Notfound></Notfound>
+             </PrivateRouter>
+          </Switch>
+        <Footer></Footer>
+       </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
